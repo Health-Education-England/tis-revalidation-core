d3aa32a7264386acc6b835f59ac6ef82c2e0b1a3
jayanta2018 <jayanta.saha@hee.nhs.uk>
2021-10-08 10:38:45 +0100

    feat: add notes create controller
    TIS21-2133

--- src/main/java/uk/nhs/hee/tis/revalidation/core/dto/TraineeNoteDto.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/core/dto/TraineeNoteDto.java
@@ -19,28 +19,23 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-package uk.nhs.hee.tis.revalidation.core.entity;
+package uk.nhs.hee.tis.revalidation.core.dto;
 
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document(collection = "notes")
-public class TraineeNote {
+public class TraineeNoteDto {
 
-  @Id
   private String id;
   private String gmcId;
   private String text;
   private LocalDateTime createdDate;
   private LocalDateTime updatedDate;
-
 }
