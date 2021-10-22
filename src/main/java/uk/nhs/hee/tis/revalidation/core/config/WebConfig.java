c38b2ae4446ec2ab3176f05d11b91d04b77edd1c
tisadmincw <tisadmincw@N2810.xhee.nhs.uk>
2021-10-22 13:56:01 +0100

    fix: allow cors pattern match

--- src/main/java/uk/nhs/hee/tis/revalidation/core/config/WebConfig.java
+++ src/main/java/uk/nhs/hee/tis/revalidation/core/config/WebConfig.java
@@ -36,7 +36,7 @@ public class WebConfig {
 
     final var config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
+    config.addAllowedOriginPattern("*");
     config.addAllowedHeader("*");
     config.addAllowedMethod("*");
 
